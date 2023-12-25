2a899bcd61d6c46f689d2947ec51f0b843b847ca
ahmed <ahmedzaid.vision@gmail.com>
2023-12-25 16:06:50 +0200

    updates

--- src/components/pages/ProjectsPage.tsx
+++ src/components/pages/ProjectsPage.tsx
@@ -5,7 +5,7 @@ import ProjecSlider from "../projectSlider";
 const Project = ({ project }) => {
   return (
     <div className="project w-full flex items-center flex-col  ">
-      <h3 className="w-fit">{project.title}</h3>
+      <h3 className="w-fit font-bold mb-3 ">{project.title}</h3>
       <div className="project-images  w-full">
         <ProjecSlider images={project.images} />
       </div>
@@ -17,7 +17,7 @@ function ProjectsPage() {
   return (
     <div className="projects mt-40 mb-10 mx-8 ">
       <div className="container mx-auto   ">
-        <div className="flex flex-wrap justify-center gap-3 ">
+        <div className="flex flex-wrap justify-center gap-20 ">
           {projects.map((project) => {
             return <Project project={project} />;
           })}
